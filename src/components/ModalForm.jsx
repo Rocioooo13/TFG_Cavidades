62c5fdf1057416fe3898696cc5bb42125fce0465
Rocío <rocioducid99@gmail.com>
2024-02-10 19:14:56 +0100

    Created method to calculate latitude and longitude

--- src/components/ModalForm.jsx
+++ src/components/ModalForm.jsx
@@ -11,6 +11,78 @@ const customStyles = {
 
 const ModalForm = ({ isOpen, onRequestClose }) => {
   // Override zIndex to display the modal overlayed to the map
+  const pruebaObtenerValor = () => {
+    // Obtener el valor del label
+    const inputValor = document.getElementById("denominacion").value;
+
+    // Imprimir el valor recuperado
+    console.log(inputValor);
+  };
+
+  const formulaLatitudLongitud = () => {
+    const aSemi = 6378137;
+    const bSemi = 6356752.314;
+    const excentricidad = 0.081819191;
+    const e1 = 0.082094438;
+    const e12 = 0.006739497;
+    const c = 6399593.626;
+
+    //Cambiar porque tengo que ver como obtener el valor de un input
+    const yDelForm = document.getElementById("y").value;
+    const hemisferioIndez =
+      document.getElementById("hemisferio1").selectedIndex;
+    const hemisferio = document.getElementById("hemisferio1")[0].label;
+    const xDelForm = document.getElementById("x").value;
+    const huso = document.getElementById("huso").value;
+
+    //Cambiar el nombre de las variable de los input
+    // COMRPOBAR DE OTRA MANERA PORQUE NO FUNCIONA
+    var yAlSurEcuador = 0;
+    if (hemisferio === "S") {
+      yAlSurEcuador = yDelForm - 10000000;
+    } else {
+      yAlSurEcuador = yDelForm;
+    }
+    const fi = yAlSurEcuador / (6366197.724 * 0.9996);
+    const ni =
+      (c / Math.pow(1 + e12 * Math.pow(Math.cos(fi), 2), 1 / 2)) * 0.9996;
+    const a = (xDelForm - 500000) / ni;
+    const A1 = Math.sin(2 * fi);
+    const A2 = A1 * Math.pow(Math.cos(fi), 2);
+    console.log("A2: ", A2);
+    const J2 = fi + A1 / 2;
+    const J4 = (3 * A1 + J2) / 4;
+    const J6 = (5 * J4 + A2 * Math.pow(Math.cos(fi), 2)) / 3;
+    const alfa = (3 / 4) * e12;
+    const beta = (5 / 3) * Math.pow(alfa, 2);
+    const gamma = (85 / 27) * Math.pow(alfa, 3);
+    const bfi = 0.9996 * c * fi - alfa * J2 + beta * J4 - gamma * J6;
+    const b = (yAlSurEcuador - bfi) / ni;
+    const zeta = ((e12 * a) / 2) * Math.pow(Math.cos(fi), 2);
+    const xi = a * (1 - zeta / 3);
+    const eta = b * (1 - zeta) + fi;
+    const senhxi = (Math.pow(Math.E, xi) - Math.pow(Math.E, -xi)) / 2;
+    const deltaLambda = Math.atan(senhxi / Math.cos(eta));
+    const tan = Math.atan(Math.cos(deltaLambda) * Math.tan(eta));
+    const meridianoCentral1 = 6 * huso - 183;
+    const radianes =
+      fi +
+      (1 +
+        e12 * Math.pow(Math.cos(fi), 2) -
+        (3 / 2) * e12 * Math.sin(fi) * Math.cos(fi) * (tan - fi)) *
+        (tan - fi);
+    const longitud1 = +(deltaLambda / Math.PI) * 180 + meridianoCentral1;
+    var longitud = 0;
+    if (meridianoCentral1 === -183) {
+      longitud = 0;
+    } else {
+      longitud = longitud1;
+    }
+    const latitud = +(radianes / Math.PI) * 180;
+
+    console.log("Longitud: ", longitud, " Latitud: ", latitud);
+    //return [longitud, latitud];
+  };
   Modal.defaultStyles.overlay.zIndex = 1000;
   return (
     <Modal
@@ -29,6 +101,7 @@ const ModalForm = ({ isOpen, onRequestClose }) => {
             className="inputForm"
             type="text"
             placeholder="Escribe el nombre de la cueva"
+            id="denominacion"
           />
         </div>
 
@@ -38,6 +111,7 @@ const ModalForm = ({ isOpen, onRequestClose }) => {
             className="inputForm"
             type="text"
             placeholder="Escribe la coordenada X de la cueva"
+            id="x"
           />
         </div>
 
@@ -47,6 +121,7 @@ const ModalForm = ({ isOpen, onRequestClose }) => {
             className="inputForm"
             type="text"
             placeholder="Escribe la coordenada Y de la cueva"
+            id="y"
           />
         </div>
 
@@ -75,6 +150,7 @@ const ModalForm = ({ isOpen, onRequestClose }) => {
             className="inputForm"
             type="text"
             placeholder="Escribe el huso de la cueva"
+            id="huso"
           />
         </div>
 
@@ -116,6 +192,7 @@ const ModalForm = ({ isOpen, onRequestClose }) => {
             className="selectForm"
             defaultChecked="true"
             placeholder="Selecciona el hemisferio al que pertenece la cueva"
+            id="hemisferio1"
           >
             <option value="Norte" selected={true}>
               N
@@ -134,6 +211,13 @@ const ModalForm = ({ isOpen, onRequestClose }) => {
         </div>
 
         <div className="botonesForm">
+          <button
+            className="botonForm"
+            type="button"
+            onClick={formulaLatitudLongitud}
+          >
+            boton prueba
+          </button>
           <button className="botonForm" type="button" onClick={onRequestClose}>
             Cancelar
           </button>
